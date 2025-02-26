afe0e02baa68dfa0368c81653e434820a8f654b9
hunter-tinkerer <reading.kabobs-9f@icloud.com>
2025-02-26 23:11:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var member1 = document.getElementById("member1");
+    var member2 = document.getElementById("member2");
+    var member3 = document.getElementById("member3");
+    var member4 = document.getElementById("member4");
+    var member5 = document.getElementById("member5");
+    var member6 = document.getElementById("member6");
+    var member7 = document.getElementById("member7");
+
+    member1.style.display = "block";
+    member2.style.display = "block";
+    member3.style.display = "block";
+    member4.style.display = "block";
+    member5.style.display = "block";
+    member6.style.display = "block";
+    member7.style.display = "block";
+    member.style.display = "block";
+}
